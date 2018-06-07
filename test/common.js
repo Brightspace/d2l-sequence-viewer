d1d2e5f1c07ba877f1e75fb226b54247ff72b4ba
Ryan Sweet <ryan.sweet@D2L.com>
2018-06-07 11:26:28 -0400

    Fix linting

--- test/common.js
+++ test/common.js
@@ -1,23 +1,23 @@
 (function() {
-    'use strict';
+	'use strict';
 
-    function timeout(ms) {
-        return new Promise( resolve => setTimeout(resolve, ms) );
-    }
+	function timeout(ms) {
+		return new Promise( resolve => setTimeout(resolve, ms) );
+	}
 
-    async function loadEntityForFixture(fileName, fixtureName, debounceTime) {
-        const entities = await fetch(fileName)
-            .then(res => res.json());
+	async function loadEntityForFixture(fileName, fixtureName, debounceTime) {
+		const entities = await fetch(fileName)
+			.then(res => res.json());
 
-        const element = fixture(fixtureName);
-        element.entity = D2L.Hypermedia.Siren.Parse(entities);
+		const element = fixture(fixtureName);
+		element.entity = D2L.Hypermedia.Siren.Parse(entities);
 
-        if (debounceTime) {
-            await timeout(debounceTime);
-        }
+		if (debounceTime) {
+			await timeout(debounceTime);
+		}
 
-        return element;
-    }
+		return element;
+	}
 
-    window.loadEntityForFixture = loadEntityForFixture;
+	window.loadEntityForFixture = loadEntityForFixture;
 })();
