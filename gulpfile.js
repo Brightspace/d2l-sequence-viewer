64a1442d8a9175b3db18fbbe85c278d62222a768
Joseph Wong <Joseph.Wong@D2L.com>
2019-01-24 13:00:47 -0500

    Polymer 3 Conversion

--- gulpfile.js
+++ gulpfile.js
@@ -4,17 +4,17 @@ const path = require('path');
 
 // merge all json files inside lang to `locales.json`
 function defaultTask() {
-    return src('lang/*.json')
-        .pipe(merge({
-            fileName: 'locales.json',
-            edit: (parsedJson, {basename}) => {
-                const lang = path.basename(basename, '.json');
-                return {
-                    [lang]: parsedJson
-                }
-            }
-        }))
-        .pipe(dest('.'));
+	return src('lang/*.json')
+		.pipe(merge({
+			fileName: 'locales.json',
+			edit: (parsedJson, {basename}) => {
+				const lang = path.basename(basename, '.json');
+				return {
+					[lang]: parsedJson
+				}
+			}
+		}))
+		.pipe(dest('.'));
 }
 
 exports.default = defaultTask;
