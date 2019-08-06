7095ec60ebc347663390a459a327a68a5ba444c3
Chris Kraljevic <Chris.Kraljevic@D2L.com>
2019-08-06 13:24:20 -0500

    moar debug

--- d2l-sequence-viewer.js
+++ d2l-sequence-viewer.js
@@ -281,7 +281,8 @@ class D2LSequenceViewer extends mixinBehaviors([
 				this._sideBarOpen();
 			}
 		}
-		if (!this._moduleProperties) {
+		console.log(`course title: ${this._moduleProperties.title}`);
+		if (!this._moduleProperties.title) {
 			this._setModuleProperties(entity);
 		}
 	}
